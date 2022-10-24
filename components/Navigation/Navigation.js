d9dd1295fbecd782f7c636552e962c27e8757705
Amil Silahic <jinobie02@gmail.com>
2022-10-23 19:16:43 -0600

    about me page added and styling

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -4,7 +4,9 @@ import { MdDarkMode } from "react-icons/md";
 
 const Navigation = () => {
   return (
-    <div className="navbar bg-[#f2f6f7] text-black font-mono">
+    // fix issue of menu staying open when in md:
+
+    <div className="navbar  text-black font-mono">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden text-xl">
@@ -15,7 +17,7 @@ const Navigation = () => {
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-white rounded-box w-52"
           >
             <li className=" text-black">
-              <Link href="/about">About</Link>
+              <Link href="/about-me">About</Link>
             </li>
 
             <li className=" text-black">
@@ -31,7 +33,7 @@ const Navigation = () => {
         <ul className="menu menu-horizontal p-0 gap-4 text-black">
           <li className="hover:bg-black hover:text-white hover:rounded-lg">
             {/* change buttons colors  */}
-            <Link href="/about">About</Link>
+            <Link href="/about-me">About</Link>
           </li>
 
           <li className="hover:bg-black hover:text-white hover:rounded-lg">
