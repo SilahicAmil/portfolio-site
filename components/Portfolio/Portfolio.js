1189d069df24d02523bcb1ab611a9133c8444d59
Amil Silahic <jinobie02@gmail.com>
2022-10-24 14:39:00 -0600

    updates to works page updating photos and etc

--- components/Portfolio/Portfolio.js
+++ components/Portfolio/Portfolio.js
@@ -1,17 +1,19 @@
+import Code from "../../public/code.avif";
 import Image from "next/image";
+import Takeya from "../../public/Takeya.jpeg";
 
 const Portfolio = () => {
   return (
     <>
-      <section className="font-mono   h-[100vh]  md:h-[100vh] sm:h-[100vh] items-center">
-        <div className=" mb-8 px-4 s:px-5 md:px-10 lg:px-[60px] h-screen">
-          <div className="py-8">
+      <section className="font-mono min-h-screen items-center ">
+        <div className=" mb-8 px-4 s:px-5 md:px-10 lg:px-[60px]">
+          <div className="py-12">
             <h2 className="mt-4 text-4xl">Portfolio</h2>
           </div>
-          <div className="grid  md:grid-cols-3  justify-center gap-5">
-            <div className="card  w-96 shadow-xl ">
+          <div className="grid md:grid-cols-2  justify-center gap-5">
+            <div className="card   shadow-xl ">
               <figure>
-                <Image src="" alt="Shoes" />
+                <Image src={Takeya} alt="takey ticket photo" />
               </figure>
               <div className="card-body shadow-xl ">
                 <h2 className="card-title text-3xl">Takeya Ticket</h2>
@@ -35,9 +37,9 @@ const Portfolio = () => {
                 </div>
               </div>
             </div>
-            <div className="card  w-96 shadow-xl">
+            <div className="card   shadow-xl">
               <figure>
-                <Image src="" alt="Shoes" />
+                <Image src={Code} alt="code" height={400} />
               </figure>
               <div className="card-body shadow-xl ">
                 <h2 className="card-title text-3xl">Takeya Ticket</h2>
@@ -58,9 +60,9 @@ const Portfolio = () => {
                 </div>
               </div>
             </div>
-            <div className="card w-96 shadow-xl ">
+            <div className="card  shadow-xl ">
               <figure>
-                <Image src="" alt="Shoes" />
+                <Image src={Code} alt="code" height={400} />
               </figure>
               <div className="card-body shadow-xl">
                 <h2 className="card-title text-3xl">Takeya Ticket</h2>
@@ -81,9 +83,9 @@ const Portfolio = () => {
                 </div>
               </div>
             </div>
-            <div className="card  w-96 shadow-xl">
+            <div className="card  shadow-xl">
               <figure>
-                <Image src="" alt="Shoes" />
+                <Image src={Code} alt="code" height={400} />
               </figure>
               <div className="card-body shadow-xl">
                 <h2 className="card-title text-3xl">Takeya Ticket</h2>
